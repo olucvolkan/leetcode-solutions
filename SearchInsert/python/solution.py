00f7c2df49503c2b2511ed79168520c36d55e3dd
Volkan <volkanoluc@gmail.com>
2019-02-01 00:56:51 +0300

    crlf fix

--- SearchInsert/python/solution.py
+++ SearchInsert/python/solution.py
@@ -1,1 +1,15 @@
-# https://leetcode.com/problems/search-insert-position/class Solution:    def search_insert(self, nums, target):        low, high = 0, len(nums)        mid = low + (high - low) / 2        while mid != low:            if nums[int(mid)] == target:                return mid            elif nums[mid] < target:                low = mid            else:                high = mid            mid = low + (high - low) / 2        return mid if target <= nums[mid] else mid + 1
+# https://leetcode.com/problems/search-insert-position/
+class Solution:
+    def search_insert(self, nums, target):
+        low, high = 0, len(nums)
+        mid = low + (high - low) / 2
+        while mid != low:
+            if nums[int(mid)] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid
+            else:
+                high = mid
+            mid = low + (high - low) / 2
+        return mid if target <= nums[mid] else mid + 1
+      

00e7d61662faab7b53bf66ea6f28957951a39b22
Volkan Oluç <volkanoluc@gmail.com>
2019-02-01 00:34:29 +0300

    .idea removed

---
+++ TwoSum/python/solution.py
@@ -0,0 +1,1 @@
+#https://leetcode.com/problems/two-sum/class Solution:    def twoSum(self,nums,target):        for i in range(len(nums)):            second_value = nums.index(target-nums[i])            if second_value:                return [i,second_value]            else:                return
